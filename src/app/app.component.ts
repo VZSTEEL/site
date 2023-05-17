bea59f1b3ebf98dfbdb0f70873542adb9d2600d8
Mihail Goloborodov <Mihail.Goloborodov@nexign.com>
2023-05-17 11:03:03 +0300

    move photos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { EMAIL, PHONE_NUMBER } from '../config';
+import { EMAIL, GALLERY, PHONE_NUMBER } from '../config';
 import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
@@ -14,25 +14,7 @@ export class AppComponent {
   title = 'vz-steel';
   PHONE_NUMBER = PHONE_NUMBER;
   EMAIL = EMAIL;
-
-  photos = [
-    'IMG-20200320-WA0005',
-    'IMG-20200320-WA0007',
-    'IMG-20200527-WA0003',
-    'IMG-20200527-WA0013',
-    'IMG-20200527-WA0019',
-    'IMG-20200605-WA0003',
-    'IMG-20200715-WA0007',
-    'IMG-20200910-WA0007',
-    'IMG-20200924-WA0001',
-    'IMG-20201019-WA0005',
-    'IMG-20201130-WA0006',
-    'IMG-20201222-WA0002',
-    'IMG-20210114-WA0001',
-    'IMG-20210324-WA0001',
-    'IMG-20210324-WA0019',
-    'IMG-20210430-WA0006'
-  ];
+  GALLERY = GALLERY;
 
   form = new FormGroup({
     fromName: new FormControl<string>(''),
--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,21 @@
 export const COMPANY_NAME = 'VZ Steel';
 export const PHONE_NUMBER = '0426186100';
 export const EMAIL = 'zolotaryov.vadim@gmail.com';
+export const GALLERY = [
+  'IMG-20200320-WA0005.jpg',
+  'IMG-20200320-WA0007.jpg',
+  'IMG-20200527-WA0003.jpg',
+  'IMG-20200527-WA0013.jpg',
+  'IMG-20200527-WA0019.jpg',
+  'IMG-20200605-WA0003.jpg',
+  'IMG-20200715-WA0007.jpg',
+  'IMG-20200910-WA0007.jpg',
+  'IMG-20200924-WA0001.jpg',
+  'IMG-20201019-WA0005.jpg',
+  'IMG-20201130-WA0006.jpg',
+  'IMG-20201222-WA0002.jpg',
+  'IMG-20210114-WA0001.jpg',
+  'IMG-20210324-WA0001.jpg',
+  'IMG-20210324-WA0019.jpg',
+  'IMG-20210430-WA0006.jpg'
+];
